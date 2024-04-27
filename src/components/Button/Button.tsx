cc29cf188eae8359387fb21def0a325526a4fc70
Khasanboy Khabibullaev <khassanboi@gmail.com>
2024-04-27 14:05:53 +0500

    feat: create button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,11 @@
+import React, { Children } from "react";
+import "./ButtonStyles.css";
+
+interface ButtonProps {
+  children: string;
+  className: string;
+}
+
+export const Button = (props: ButtonProps) => {
+  return <button className={"btn " + props.className}>{props.children}</button>;
+};
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,14 @@
 import React from "react";
 import "./HeaderStyles.css";
+import { Button } from "../Button/Button";
 
 export const Header = () => {
   return (
     <header className="header">
       <span className="header__logo">ASYNC RACE</span>
       <nav className="header__ui">
-        <button className="header__button btn btn--large btn--red">
-          Garage
-        </button>
-        <button className="header__button btn btn--large btn--blue">
-          Winners
-        </button>
+        <Button className="btn--red">Garage</Button>
+        <Button className="btn--blue">Winners</Button>
       </nav>
     </header>
   );
