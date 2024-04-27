bdf7c4af2c28b1ee154373a1af2dafa1a8c3e17b
Khasanboy Khabibullaev <khassanboi@gmail.com>
2024-04-27 13:33:03 +0500

    init: start async-race project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Async Race</h1>
+      <h4>Project Init</h4>
+    </div>
+  );
+}
+
+export default App;
