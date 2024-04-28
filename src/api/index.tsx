3db8712b2fba4bedee1e1e12a930b99ab2c3f66c
Khasanboy Khabibullaev <khassanboi@gmail.com>
2024-04-28 14:03:10 +0500

    feat: create get-car api request

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -4,3 +4,9 @@ const garageUrl = 'http://localhost:3000/garage';
 const winnersUrl = 'http://localhost:3000/winners';
 
 export const getCars = () => axios.get(garageUrl);
+export const getCar = (id: number) =>
+  axios.get(garageUrl, {
+    params: {
+      id,
+    },
+  });
