cbc60a8abde371ffb70ffb5e2dabff918131e35f
Khasanboy Khabibullaev <khassanboi@gmail.com>
2024-04-28 14:38:36 +0500

    feat: create get-winner api request

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -4,20 +4,13 @@ const garageUrl = 'http://localhost:3000/garage';
 const winnersUrl = 'http://localhost:3000/winners';
 
 export const getCars = () => axios.get(garageUrl);
-export const getCar = (id: number) =>
-  axios.get(garageUrl, {
-    params: {
-      id,
-    },
-  });
+export const getCar = (id: number) => axios.get(`${garageUrl}/${id}`);
 export const createCar = (name: string, color: string) =>
   axios.post(garageUrl, {
     name,
     color,
   });
-
 export const deleteCar = (id: number) => axios.delete(`${garageUrl}/${id}`);
-
 export const updateCar = (id: number, name: string, color: string) => {
   axios.put(`${garageUrl}/${id}`, {
     name,
@@ -26,3 +19,4 @@ export const updateCar = (id: number, name: string, color: string) => {
 };
 
 export const getWinners = () => axios.get(winnersUrl);
+export const getWinner = (id: number) => axios.get(`${winnersUrl}/${id}`);
